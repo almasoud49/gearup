11a4008b9374d19b37f6bfec33a7d1e47d8b6022
Abdullah Al Masoud <almasoud49@yahoo.com>
2026-07-06 09:45:28 +0600

    [DONE] config file and middleware part

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
 
 const app : Application = express();
 
 
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
 app.get("/",(req : Request, res : Response) => {
-    res.send("Hello, World!");
+    res.send("Welcome to my gearup web application.");
 });
 
 
 export default app;
+
+
