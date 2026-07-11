50ff67da0a459044ddff5158a4fa17ff2144c6f2
Abdullah Al Masoud <almasoud49@yahoo.com>
2026-07-11 17:00:53 +0600

    [DONE] pisma config updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,10 @@
-
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+// prisma.config.ts
+import { defineConfig } from 'prisma/config';
+import 'dotenv/config';
 
 export default defineConfig({
-  schema: "prisma/schema",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
+    schema: 'prisma/schema/schema.prisma',
+    datasource: {
+        url: process.env.DATABASE_URL!,
+    },
 });
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from '@prisma/client';
 
+declare global {
+    var prisma: PrismaClient | undefined;
+}
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-
-
-export const prisma = new PrismaClient({
-  adapter,
-  datasourceUrl: process.env.DATABASE_URL, 
+const prisma = global.prisma || new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prisma;
+}
+
+export default prisma;
